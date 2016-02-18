8b50112294433222a474308b15aa3653bbec8166
UncleChar <xhijob@126.com>
2016-02-18 17:13:54 +0800

    first login

---
+++ OutsourcingProjectTests/OutsourcingProjectTests.m
@@ -0,0 +1,39 @@
+//
+//  OutsourcingProjectTests.m
+//  OutsourcingProjectTests
+//
+//  Created by LingLi on 16/2/18.
+//  Copyright © 2016年 LingLi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OutsourcingProjectTests : XCTestCase
+
+@end
+
+@implementation OutsourcingProjectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
